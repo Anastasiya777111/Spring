a6a9afc4677d57011ddeecb30dcfe895c0d3011e
Anastasiya <avojtesik1@gmail.com>
2021-12-03 18:28:32 +0300

    add search form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const cardBox = document.querySelector('.mainContent .containerMain .displ1');
 
+const searchInput = document.querySelector('#search');
+
 function getCardHtml(el) {
     return `
         <a class="card shadow">
@@ -16,6 +18,20 @@ function getCardHtml(el) {
     `;
   }
 
+ function searchFilter(result) {
+  result.forEach(el => {
+       cardBox.insertAdjacentHTML('afterbegin', getCardHtml(el));
+    });
+  }
+  
+  function errorString() {
+      cardBox.insertAdjacentHTML('afterbegin', `<p class="noResults">No results</p>`);
+  }
+
+  function clearCardBox() {
+     cardBox.innerHTML = '';
+  }
+
   function renderFirst() {
     for (let i = 0; i < data.length; i++) {
       const cardHtml = getCardHtml(data[i]);
@@ -24,4 +40,22 @@ function getCardHtml(el) {
   }
   
   renderFirst();
+
+  searchInput.addEventListener('keyup', e => {
+    const result = data.filter(word =>
+      word.title.toLowerCase().includes(e.target.value.toLowerCase())||
+      word.desc.toLowerCase().includes(e.target.value.toLowerCase())    
+    );
+    setTimeout(clearCardBox, 300)
+    if(result.length != 0){
+      setTimeout(searchFilter, 300, result);}else{
+       setTimeout(errorString, 300);
+    }
+    if (e.target.value == '') {
+      clearCardBox();
+      renderFirst();
+    }
+  
+  });
+
   
